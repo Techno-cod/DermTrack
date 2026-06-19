8601c8a8359de691e69c2e2b3d6c86dd8eb480cb
Palak Shivhare <palakshivhare213@gmail.com>
2026-06-19 19:40:04 +0530

    Fix auth service API URL

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5001/api/auth";
+const API_URL =
+  `${import.meta.env.VITE_API_URL}/auth`;
 
 export const loginUser = async (email, password) => {
   const response = await axios.post(
